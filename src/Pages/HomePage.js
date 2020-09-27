e2aa2af879aa7ee3d802cb340aebf684d7410b5d
Daniel Binoy <dbinoy15@gmail.com>
2020-09-27 10:42:30 -0700

    create

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const HomePage = () => (
+    <>
+        <h1>Hello, this is Binoy's blog</h1>
+        <p>
+            Never thought I'd make it. Three days grade school,
+            three days high school. Those were awkward. Three
+            days college. I'm glad I took a day and hitchhiked
+            around the hive. You did come back different.
+        </p>
+        <p>
+            We know that you, as a bee, have worked your whole 
+            life to get to the  point where you can work for 
+            your whole life. Honey begins when our valiant Pollen 
+            Jocks bring the nectar to the hive. Our top-secret 
+            formula is automatically  color-corrected, scent-adjusted 
+            and bubble-contoured into this soothing sweet syrup 
+            with its distinctive golden glow you know as Honey!
+        </p>
+
+    </>
+);
+
+export default HomePage;
