f7be92e668a0eea1ab522eb2d2f1b3a8661578c5
Daniel Binoy <dbinoy15@gmail.com>
2020-09-27 11:37:31 -0700

    articles list links to article

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <div id="page-body">
           <Route path="/" component={HomePage} exact />
           <Route path="/about" component={AboutPage} />
-          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
           <Route path="/article/:name" component={ArticlePage} />
         </div>
       </div>
--- src/Pages/ArticlesList.js
+++ src/Pages/ArticlesList.js
@@ -1,24 +1,18 @@
 import React from 'react';
-
-
+import articleContent from './article-content';
+import {Link} from 'react-router-dom';
 const ArticlesList = () => (
     <>
         <h1>Article List</h1>
-        <p>
-            Never thought I'd make it. Three days grade school,
-            three days high school. Those were awkward. Three
-            days college. I'm glad I took a day and hitchhiked
-            around the hive. You did come back different.
-        </p>
-        <p>
-            We know that you, as a bee, have worked your whole 
-            life to get to the  point where you can work for 
-            your whole life. Honey begins when our valiant Pollen 
-            Jocks bring the nectar to the hive. Our top-secret 
-            formula is automatically  color-corrected, scent-adjusted 
-            and bubble-contoured into this soothing sweet syrup 
-            with its distinctive golden glow you know as Honey!
-        </p>
+        {
+            articleContent.map(
+                (article, key)=>
+                <Link key={key} to = {"/article/"+article.name} >
+                <h3>{article.title}</h3>
+                </Link>
+            
+            )
+        }
         
 
     </>
