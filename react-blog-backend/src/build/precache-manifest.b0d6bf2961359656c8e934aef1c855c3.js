05cd189ebcc2b4d0a9b209a62b67191a985e8269
Daniel Binoy <dbinoy15@gmail.com>
2020-09-28 14:23:08 -0700

    build, backend

---
+++ react-blog-backend/src/build/precache-manifest.b0d6bf2961359656c8e934aef1c855c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d683c287fe4f6ef5d6585dda534ee59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a129a1df2d178cd73f02",
+    "url": "/static/css/main.2362a757.chunk.css"
+  },
+  {
+    "revision": "bd4847e716b948445573",
+    "url": "/static/js/2.1aa6943c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1aa6943c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a129a1df2d178cd73f02",
+    "url": "/static/js/main.35ea2458.chunk.js"
+  },
+  {
+    "revision": "4c1fe02794de59eabee5",
+    "url": "/static/js/runtime-main.ba6060d1.js"
+  }
+]);
