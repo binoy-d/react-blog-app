10f14dbbad66ba2753bf230f616e9893fbeeb47b
Daniel Binoy <dbinoy15@gmail.com>
2020-09-28 12:58:26 -0700

    withdb

--- react-blog-backend/src/server.js
+++ react-blog-backend/src/server.js
@@ -7,35 +7,47 @@ const app = express();
 app.use(bodyParser.json());
 
 
-app.get('/api/articles/:name', async (req, res) => {
+const withDB = async (operations) => {
     try {
-        const articleName = req.params.name;
         //returns client object to use to send queries
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
         const db = client.db('react-blog');
-        const articleInfo = await db.collection('articles').findOne({ name: articleName });
 
+        await operations(db);
 
-        res.status(200).json(articleInfo);
         client.close();
     } catch (e) {
         res.status(500).json({ message: 'bruh', e });
     }
+}
 
+app.get('/api/articles/:name', async (req, res) => {
+
+    withDB(async (db) => {
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(articleInfo);
+    });
 });
 
 
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-    const articleName = req.params.name;
-
-    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-    const db = client.db('react-blog');
+app.post('/api/articles/:name/upvote', async (req, res) => {
 
 
-    const articleinfo = await db.collection('articles').findOne({name:articleName});
+    withDB( async (db)=>{
+        const articleName = req.params.name;
+        const articleinfo = await db.collection('articles').findOne({ name: articleName });
 
-    await db.collection('articles').updateOne({name:articleName}, '$set':{upvotes:articleinfo.upvotes+1})
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': { upvotes: articleinfo.upvotes + 1 }
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+
+        res.status(200).json(updatedArticleInfo);
+    });
+        
+        
 
 });
 
