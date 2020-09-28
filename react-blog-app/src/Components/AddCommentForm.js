821a27fbc32a481a5f47c868b3fc13619e8b4b2d
Daniel Binoy <dbinoy15@gmail.com>
2020-09-28 14:14:03 -0700

    Update AddCommentForm.js

--- react-blog-app/src/Components/AddCommentForm.js
+++ react-blog-app/src/Components/AddCommentForm.js
@@ -2,11 +2,27 @@ import React, { useState } from 'react';
 
 
 
-const AddCommentForm = () => {
+const AddCommentForm = ({articleName, setArticleInfo}) => {
 
     const [username, setUsername] = useState("");
     const [commentText, setCommentText] = useState("");
 
+
+    const addComment = async ()=>{
+        const result = await fetch('/api/articles/'+articleName+'/add-comment', {
+            method: 'post',
+            body: JSON.stringify({username, text: commentText}),
+            headers: {
+                'Content-Type':'application/json',
+            }
+        });
+
+
+        const body = await result.json();
+
+
+        setArticleInfo(body)
+    }
     return (
         <div id="add-comment-form">
             <h3>Add a comment</h3>
