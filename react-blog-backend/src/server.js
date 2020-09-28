6b8f45a11851e90b2f987d339d9b32525a76d375
Daniel Binoy <dbinoy15@gmail.com>
2020-09-28 11:25:55 -0700

    NODE

--- react-blog-backend/src/server.js
+++ react-blog-backend/src/server.js
@@ -1,36 +1,46 @@
-import express from 'express'
+import express from 'express';
 import bodyParser from 'body-parser';
-
+import { MongoClient } from 'mongodb';
 const app = express();
 
-const articlesInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments: [],
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments: [],
-    },
-    'my-thoughts-on-resumes': {
-        upvotes: 0,
-        comments: [],
+
+app.use(bodyParser.json());
+
+
+app.get('/api/articles/:name', async (req, res) => {
+    try {
+        const articleName = req.params.name;
+        //returns client object to use to send queries
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('react-blog');
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+
+
+        res.status(200).json(articleInfo);
+        client.close();
+    } catch (e) {
+        res.status(500).json({ message: 'bruh', e });
     }
 
-}
-app.use(bodyParser.json());
+});
+
+
 
 app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
-    articlesInfo[articleName].upvotes++;
-    res.status(200).send(articleName + ' now has ' + articlesInfo[articleName].upvotes + ' upvotes');
+
+    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+    const db = client.db('react-blog');
+
+
+    const articleinfo = await db.collection('articles').findOne({name:articleName});
+
+    await db.collection('articles').updateOne({name:articleName}, '$set':{upvotes:articleinfo.upvotes+1})
+
 });
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
-    const {username, text} = req.body;
-    articlesInfo[req.params.name].comments.push({username, text});
-
-    res.status(200).send(articlesInfo[req.params.name]);
+    const { username, text } = req.body;
 });
 
 
